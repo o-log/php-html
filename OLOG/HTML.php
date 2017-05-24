f4d63f016d8dc8175c0369719248a29dc5f239c2
Антон Пинегин <a.pinegin@corp.mail.ru>
2017-05-24 18:03:36 +0300

    Fix

--- OLOG/HTML.php
+++ OLOG/HTML.php
@@ -18,9 +18,6 @@ class HTML
 
 		$tag_attributes = '';
 		foreach ($tag_attribute_arr as $tag_attribute => $tag_attribute_str) {
-			if ($tag_attribute_str == '') {
-				continue;
-			}
 			$tag_attributes .= ' ' . Sanitize::sanitizeAttrValue($tag_attribute) . '="' . Sanitize::sanitizeAttrValue($tag_attribute_str) . '" ';
 		}
 
@@ -29,25 +26,7 @@ class HTML
 
 	public static function echoTag($tag_name, $tag_attribute_arr = [], $html)
 	{
-		if ($tag_name == '') {
-			return '';
-		}
-
-		if (is_callable($html)) {
-			ob_start();
-			$html();
-			$html = ob_get_clean();
-		}
-
-		$tag_attributes = '';
-		foreach ($tag_attribute_arr as $tag_attribute => $tag_attribute_str) {
-			if ($tag_attribute_str == '') {
-				continue;
-			}
-			$tag_attributes .= ' ' . Sanitize::sanitizeAttrValue($tag_attribute) . '="' . Sanitize::sanitizeAttrValue($tag_attribute_str) . '" ';
-		}
-
-		echo '<' . Sanitize::sanitizeAttrValue($tag_name) . ' ' . $tag_attributes . '>' . $html . '</' . Sanitize::sanitizeAttrValue($tag_name) . '>';
+		echo self::tag($tag_name, $tag_attribute_arr, $html);
 	}
 
 	public static function a($url, $text, $classes_str = '')
