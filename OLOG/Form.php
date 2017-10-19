4058a5015e72d0e6757e62fd29157f0857776288
ologinov <olognv@gmail.com>
2017-10-19 09:30:28 +0300

    1

--- OLOG/Form.php
+++ OLOG/Form.php
@@ -6,17 +6,51 @@ class Form
 {
     const FIELD_NAME_OPERATION_CODE = '_OPERATION_CODE';
 
-    public static function op($operation_code)
+    static public function form($content, string $action, string $method = 'post', array $classes_arr = [], array $attrs_arr = []){
+        if (is_callable($content)) {
+            ob_start();
+            $content();
+            $content_html = ob_get_clean();
+        } else {
+            $content_html = $content;
+        }
+
+        if (!empty($classes_arr)){
+            $classes_str = implode(' ', $classes_arr);
+            $attrs_arr['class'] = $classes_str;
+        }
+
+        $attrs_arr['action'] = $action;
+        $attrs_arr['method'] = $method;
+
+        echo HTML::tag('form', $attrs_arr, $content_html);
+    }
+
+    static public function input(string $name, string $value, string $type = 'text', array $classes_arr = [], array $attrs_arr = [])
+    {
+        $attrs_arr['name'] = $name;
+        $attrs_arr['value'] = $value;
+        $attrs_arr['type'] = $type;
+
+        if (!empty($classes_arr)){
+            $classes_str = implode(' ', $classes_arr);
+            $attrs_arr['class'] = $classes_str;
+        }
+
+        echo HTML::tag('input', $attrs_arr, '');
+    }
+
+    static public function op($operation_code)
     {
         return '<input type="hidden" name="' . self::FIELD_NAME_OPERATION_CODE . '" value="' . HTML::attr($operation_code) . '">';
     }
 
-    public static function match(string $operation_code, callable $callback_arr, string $field = self::FIELD_NAME_OPERATION_CODE)
+    static public function match(string $operation_code, callable $callback, string $field = self::FIELD_NAME_OPERATION_CODE)
     {
         // Messages::POST not used to minimize dependencies
         if (isset($_POST[$field])) {
             if ($_POST[$field] == $operation_code) {
-                call_user_func($callback_arr);
+                call_user_func($callback);
             }
         }
     }
--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 require_once '../vendor/autoload.php';
 
+use OLOG\Form;
+
 echo \OLOG\HTML::a('/', 'Home');
 
 echo \OLOG\HTML::div('class', '', 'div');
@@ -18,6 +20,11 @@ echo \OLOG\HTML::tag(
 echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>';
 
 $create_form_element_id = 'collapse_' . rand(1, 999999);
+
+Form::form(function(){
+    Form::input('test', 'bla');
+    Form::input('', 'submit', 'submit');
+}, '/');
 
 echo \OLOG\MagnificPopup::button($create_form_element_id, '', 'открыть попап');
 
